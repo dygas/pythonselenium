f836836ebde0e6ac534590a8bb7751c5398a1aae
dygas <grzegorz.dygas@hotmail.com>
2020-03-31 14:16:21 +0200

    20 lekcja

---
+++ nazwy_zmiennych.py
@@ -0,0 +1,8 @@
+liczba = 1
+pierwszaliczba = 1
+_pierwszaliczba =2
+_liczba = 2
+liczbA = 2000000;
+# wszystkie zmienne w python są casesensitive znaczy to tyle,że każda wielkość litera oznacza,że to inna zmienna
+#nie robimyt cegoś takiego
+1_liczba = 90; # jest to błąd
